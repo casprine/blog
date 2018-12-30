8b4ebdf4cd0365c0c951a8dfc23126fa96d5f1f0
Casprine <casprine.001@gmail.com>
2018-12-30 23:01:27 +0000

    removed next/sass

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app
 
     server.listen(4000, err => {
       if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
+      console.log("> Ready on http://localhost:4000");
     });
   })
   .catch(ex => {
