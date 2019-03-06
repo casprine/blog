46aad3375658db02100c963b2c5ada46fd2ff40e
casprine <casprine.001@gmail.com>
2019-03-06 16:16:54 +0000

    [CHANGE] fixed merge conflict in index.js

--- src/context/index.js
+++ src/context/index.js
@@ -1,23 +1,12 @@
 import React, { Component, Fragment } from "react";
 import { day } from "../utils/helpers";
-import data from "../../posts.json";
-
 const ContextContext = React.createContext();
 
 class ContextProvider extends Component {
   state = {
     dark: !day,
-    artiles: null,
-    currentArticle: null,
     footer: false
   };
-
-  componentWillMount() {
-    const posts = Object.values(data);
-    this.setState({
-      articles: posts
-    });
-  }
 
   toggleTheme = () => {
     this.state.dark
@@ -31,22 +20,15 @@ class ContextProvider extends Component {
       : this.setState({ footer: true });
   };
 
-  fetchById = id => {
-    console.log(this.state.articles, id);
-    // return id;
-  };
-
   render() {
-    const { dark, articles, footer } = this.state;
+    const { darky, footer } = this.state;
     return (
       <Fragment>
         <ContextContext.Provider
           value={{
             theme: dark,
             footer: footer,
-            articles: articles,
             toggleTheme: this.toggleTheme,
-            fetchById: this.fetchById,
             toggleFooter: this.toggleFooter
           }}
         >
